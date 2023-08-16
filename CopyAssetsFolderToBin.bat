e1efbd5b2142e4b257b5c42594f5087cd943afe9
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-08-16 21:14:58 +0530

    Moved GUI assets to Resources folder

--- CopyAssetsFolderToBin.bat
+++ CopyAssetsFolderToBin.bat
@@ -5,4 +5,8 @@ REM Copying Assets folder to build
 xcopy "Assets" "build\x64\debug\Assets" /E /I /Y
 xcopy "Assets" "build\x64\debug\bin\Assets" /E /I /Y
 
+REM Copying Resources folder to build 
+xcopy "Resources" "build\x64\debug\Resources" /E /I /Y
+xcopy "Assets" "build\x64\debug\bin\Resources" /E /I /Y
+
 PAUSE
