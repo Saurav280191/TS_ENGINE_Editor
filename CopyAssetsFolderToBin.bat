c79523ed37941650bb57f3ac62a4d7c73abde420
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-08-16 21:23:10 +0530

    Delete Gui folder from Assets folder. Mode modification to batch file to copy Resources.

--- CopyAssetsFolderToBin.bat
+++ CopyAssetsFolderToBin.bat
@@ -7,6 +7,6 @@ xcopy "Assets" "build\x64\debug\bin\Assets" /E /I /Y
 
 REM Copying Resources folder to build 
 xcopy "Resources" "build\x64\debug\Resources" /E /I /Y
-xcopy "Assets" "build\x64\debug\bin\Resources" /E /I /Y
+xcopy "Resources" "build\x64\debug\bin\Resources" /E /I /Y
 
 PAUSE
