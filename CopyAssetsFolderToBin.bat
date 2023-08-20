a72366f2782959733177884b7736dbc2a85058c2
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-08-21 01:17:38 +0530

    Add batch command to remove existing Assets and Resources folder before copying

--- CopyAssetsFolderToBin.bat
+++ CopyAssetsFolderToBin.bat
@@ -1,7 +1,14 @@
 @echo off
 
-REM Copying Assets folder to build 
+REM Delete all file from target Assets folder
+rmdir /s /q "build\x64\debug\Assets"
+rmdir /s /q "build\x64\debug\bin\Assets"
 
+REM Delete all file from target Resources folder
+rmdir /s /q "build\x64\debug\Resources"
+rmdir /s /q "build\x64\debug\bin\Resources"
+
+REM Copying Assets folder to build 
 xcopy "Assets" "build\x64\debug\Assets" /E /I /Y
 xcopy "Assets" "build\x64\debug\bin\Assets" /E /I /Y
 
